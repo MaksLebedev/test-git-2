d96662734410d34432534fbbfeece253990275f1
Maksim Lebedev <monk527@yandex.ru>
2024-04-17 13:24:34 +0500

    made changes in master

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-console.log("Hello, GIT!");
+console.log("Hello, World!");
 
-console.log('Hello, World!');
+const userAge = 35;
+if (userAge >= 18) {
+  console.log("Вы совершеннолетний");
+} else {
+  console.log("Вы не совершеннолетний");
+}
